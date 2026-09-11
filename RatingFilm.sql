a8ec6fa8a4372ff2b6bcf2b677b86098c530a100
akmalnezu <akmalnuzulla@gmail.com>
2026-09-12 01:04:17 +0800

    Initial commit

---
+++ RatingFilm.sql
@@ -0,0 +1,100 @@
+create table user (
+	id int primary key auto_increment,
+	username varchar (50),
+	email varchar (100),
+	password varchar (100),
+	create_at datetime default CURRENT_TIMESTAMP(),
+	update_at datetime default CURRENT_TIMESTAMP() on update CURRENT_TIMESTAMP()
+);
+
+create table admin (
+	id int primary key auto_increment,
+	username varchar (50),
+	password varchar (100),
+	create_at datetime default CURRENT_TIMESTAMP()
+);
+
+create table film (
+	id int primary key auto_increment,
+	admin_id int,
+	judul varchar (100),
+	genre varchar (100),
+	sutradara varchar (100),
+	pemeran varchar (100),
+	tahun_rilis int,
+	sinopsis varchar (100),
+	create_at datetime default CURRENT_TIMESTAMP(),
+	update_at datetime default CURRENT_TIMESTAMP() on update CURRENT_TIMESTAMP(),
+	foreign key (admin_id) references admin(id)
+);
+
+create table rating (
+	id int primary key auto_increment,
+	user_id int,
+	film_id int,
+	nilai int check (nilai >= 1 and nilai <= 10),
+	create_at datetime default CURRENT_TIMESTAMP(),
+	update_at datetime default CURRENT_TIMESTAMP() on update CURRENT_TIMESTAMP(),
+	foreign key (user_id) references user(id),
+	foreign key (film_id) references film(id)
+);
+
+create table review (
+	id int primary key auto_increment,
+	user_id int,
+	film_id int,
+	isi_review varchar(250),
+	create_at datetime default CURRENT_TIMESTAMP(),
+	update_at datetime default CURRENT_TIMESTAMP() on update CURRENT_TIMESTAMP(),
+	foreign key (user_id) references user(id),
+	foreign key (film_id) references film(id)
+);
+
+create table watchlist (
+	id int primary key auto_increment,
+	user_id int,
+	film_id int,
+	create_at datetime default CURRENT_TIMESTAMP(),
+	foreign key (user_id) references user(id),
+	foreign key (film_id) references film(id)
+);
+
+insert into user (username, email, password) values
+('Ariel', 'ariel@gmail.com', 'ariel123'),
+('Bani', 'bani@gmail.com', 'bani123'),
+('Ferdi', 'ferdi@gmail.com', 'ferdi123');
+
+insert into admin (username, password) values
+('admin1', 'admin1123'),
+('admin2', 'admin2134');
+
+insert into film (admin_id, judul, genre, sutradara, pemeran, tahun_rilis, sinopsis) values
+('')
+
+insert into rating (user_id, film_id, nilai) values
+('')
+
+insert into review (user_id, film_id, isi_review) values 
+('')
+
+insert into watchlist (user_id, film_id) values 
+('')
+
+create
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
